38e3c28d0d20dcdb80f8b719967ff23743928fb5
shiva panda <shivaprasadatwork@gmail.com>
2025-05-30 14:46:43 +0530

    Added Paginator

--- services/product-service/controllers/productController.js
+++ services/product-service/controllers/productController.js
@@ -4,11 +4,28 @@ const Product = require('../models/Product')
 //! api to get all products
 exports.getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find();
-        res.status(200).json(products)
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+
+        const products = await Product.find()
+            .skip(skip)
+            .limit(limit);
+
+        const total = await Product.countDocuments();
+
+        res.status(200).json({
+            page,
+            limit,
+            totalPages: Math.ceil(total / limit),
+            totalItems: total,
+            products,
+        })
     } catch (error) {
         console.error()
-        res.status(500).json({ msg: 'No Records Found'})
+        res.status(500).json({ msg: 'No Records Found' })
     }
 }
 
@@ -19,13 +36,13 @@ exports.getFeaturedProducts = async (req, res) => {
 }
 
 //! api to get all categories
-exports.getCategories = async(req, res) => {
+exports.getCategories = async (req, res) => {
     const categories = await Product.distinct("category")
     res.status(200).json(categories)
 }
 
 //! api to get all offers
-exports.getOffers = async(req, res) => {
-    const offers = await Product.find({ discount: { $gt: 0 }})
+exports.getOffers = async (req, res) => {
+    const offers = await Product.find({ discount: { $gt: 0 } })
     res.status(200).json(offers)
 }
